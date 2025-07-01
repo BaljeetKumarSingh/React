6b5a2eed2050df2c18f27787d9a1fc2c9ab5fec1
Baljeet Kumar <baljeetkumar2803@gmail.com>
2025-07-01 16:58:06 +0530

    Added Filter Restaurant feature using search bar

--- src/components/Body.js
+++ src/components/Body.js
@@ -15,12 +15,20 @@ function fetchRestaurantList(data) {
 }
 
 const Body = () => {
+  // console.log("Body Rendered");
 
   // local state variable - Super powerful variable
   // this is one of the hoock that react provide to us
   // whenever this state variable get updated, react re-render its component
   // react is keeping an eye on this use state variable
-  const [listOfRestaurant, setListOfRestaurant] = useState([]);
+  const [listOfRestaurant, setListOfRestaurant] = useState([]); // this is original list of res, only update once while getting it from api
+
+  // this will act as a copy of list of res, and used to filter res from original list of res
+  const [filteredRestaurant, setFilteredRestaurant] = useState([]);
+
+  const [searchText, setSearchText] = useState("");
+
+  // whenever state variable updates, react triggers a reconciliation cycle (re-render the component)
 
   // Api failled
 
@@ -40,9 +48,9 @@ const Body = () => {
   // this is another hoock that react provide to us.
   // it takes two parameter callback function and dependency array
   // super power: as soon as rendering process is finished it call this callback function.
-  // load -> Render -> API -> Re-render 
+  // load -> Render -> API -> Re-render
 
-  useEffect(() =>{
+  useEffect(() => {
     getRestaurantList();
   }, []);
 
@@ -50,22 +58,42 @@ const Body = () => {
     const list = await fetchRestaurantList(resList);
     // console.log(list);
     setListOfRestaurant(list);
-  }
+    setFilteredRestaurant(list);
+    // after this line body get rendered and update above both state variable in one render.(can see by debuging)
+  };
 
   // Conditional Rendering
   // if (listOfRestaurant.length === 0) {
   //   return <Shimmer />;
   // }
 
-  return listOfRestaurant.length === 0 ? <Shimmer /> : (
+  return listOfRestaurant.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body-container">
       <div className="search-bar">
         <input
           type="text"
           placeholder="Search Your Fav Restaurant"
           className="text-input"
+          value={searchText} // this is read only type
+          // this will help us to change the searchText and thus make it editable
+          onChange={(e) => {
+            setSearchText(e.target.value); // and on every key press its changes state variable by re-rendering the entire body component
+          }}
         />
-        <button className="search-btn">Search</button>
+        <button
+          className="search-btn"
+          onClick={() => {
+            // Filter the cards based of the searchText(input value)
+            const searchFilteredList = listOfRestaurant.filter((res) =>
+              res.info.name.toLowerCase().includes(searchText.toLowerCase())
+            );
+            setFilteredRestaurant(searchFilteredList);
+          }}
+        >
+          Search
+        </button>
       </div>
       <button
         className="filter-btn"
@@ -73,13 +101,13 @@ const Body = () => {
           const filteredList = listOfRestaurant.filter(
             (res) => res.info.rating.rating_text >= 4.0
           );
-          setListOfRestaurant(filteredList);
+          setFilteredRestaurant(filteredList);
         }}
       >
         Top Rated Restaurant
       </button>
       <div className="res-container">
-        {listOfRestaurant.map((restaurant) => (
+        {filteredRestaurant.map((restaurant) => (
           <RestaurantCard key={restaurant.info.resId} resData={restaurant} />
         ))}
       </div>
